2b84e031c60887b1d3cf853e1d2617c2258126fa
Akas_Datta <012akasdatta@gmail.com>
2024-01-12 00:16:24 +0600

    splice, concat, push, map method added

--- 1simple-javascript/array.js
+++ 1simple-javascript/array.js
@@ -22,3 +22,40 @@ const fruits = ["Banana", "apple", "mango"];
 
 const result = fruits.slice(1, 4);
 console.log(result);
+
+
+// splice
+const fruits = ["Banana", "apple", "mango"];
+
+// const result = fruits.splice(index, numberOfRemovedItems);
+const result = fruits.splice(2, 2, "Pineapple", "Frout", "Frout2");
+console.log(result);
+
+
+// concat
+const fruits = ["Banana", "apple", "mango"];
+
+// const result = fruits.splice(index, addlasttwoItems);
+const result = fruits.concat("Frout", "Frout2");
+console.log(result);
+
+
+// push
+const fruits = ["Banana", "apple", "mango"];
+
+const result = fruits.push("Frout");
+console.log(result);
+
+// map
+const fruits = ["Banana", "apple", "mango"];
+
+const result = fruits.map((f) => {
+    if (f === "Apple"){
+        return "Apple";
+    }
+    else{
+        return "N/A";
+    }
+});
+console.log(result);
+console.log(fruits);
