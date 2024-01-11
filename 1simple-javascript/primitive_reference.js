185a14913d21eb97fe2a226023caa5be01d70860
Akas_Datta <012akasdatta@gmail.com>
2024-01-11 23:10:46 +0600

    difference between premitive and reference done

--- 1simple-javascript/primitive_reference.js
+++ 1simple-javascript/primitive_reference.js
@@ -1,7 +1,19 @@
 // primitive type
-let x = 5;
-let y = 6;
-x = y;
-y = 8;
-console.log(x);
-console.log(y);
+// let x = 5;
+// let y = 6;
+// x = y;
+// y = 8;
+// console.log(x);
+// console.log(y);
+
+let a = ["JS", "Python"];
+let b = ["TS", "C++"];
+
+b = a;
+
+console.log(a);
+console.log(b);
+
+a.push("Go");
+console.log(a);
+console.log(b);
