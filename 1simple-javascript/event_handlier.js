c65a9666a57b5bfdc26a8a1832abf43937296e06
Akas_Datta <012akasdatta@gmail.com>
2024-01-11 22:57:56 +0600

    event handlier done and premitive reference added

--- 1simple-javascript/event_handlier.js
+++ 1simple-javascript/event_handlier.js
@@ -1,3 +1,13 @@
+/*
 function hello(){
     console.log("hello world!");
 }
+*/
+const button = document.getElementById("button");
+
+// event handlier
+function hello(){
+    console.log("Hello world");
+}
+
+button.addEventListener("click", hello);
